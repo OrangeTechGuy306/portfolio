d0a434d8b42c5f3aef69b9988f7658690aeb382e
orange <orangetechguy@gmail.com>
2023-06-22 19:35:27 +0100

    implementing route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Home from './pages/home/Home';
 import {Routes, Route} from "react-router-dom"
+import Dash from "./admin/dashboard/Dash"
+import Login from "./admin/login/Login"
 
 function App() {
   return (
   
      <Routes>
         <Route path='/' element={<Home/>} />
+        <Route path='/adminlogin' element={<Login/>} />
+        <Route path='/admindash' element={<Dash/>} />
      </Routes>
   )
 }
